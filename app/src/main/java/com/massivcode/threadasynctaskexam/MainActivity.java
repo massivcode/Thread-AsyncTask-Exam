268424fa65c78560f5c1650a4de011cf366574bf
prChoe <massivcode@gmail.com>
2015-11-23 10:42:04 +0900

    Handler에서 Runnable 객체를 사용하지 않고 handleMessage 함수를 재정의하여 UI 업데이트하기.

--- app/src/main/java/com/massivcode/threadasynctaskexam/MainActivity.java
+++ app/src/main/java/com/massivcode/threadasynctaskexam/MainActivity.java
@@ -13,8 +13,24 @@ public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     TextView mCountTextView = null;
 
+    // 해당 메시지가 무엇을 처리하는지 구분하기 위한 상수
+    static final private int MESSAGE_DRAW_CONTENT_COUNT = 1;
+
     // 1. 메시지 큐에 메시지를 추가하기 위한 핸들러를 생성한다.
-    Handler mHandler = new Handler();
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_DRAW_CONTENT_COUNT: {
+                    int currentCount = msg.arg1;
+                    TextView countTextView = (TextView)msg.obj;
+
+                    countTextView.setText("Count : " + currentCount);
+                    break;
+                }
+            }
+        }
+    };
 
 
     @Override
@@ -24,26 +40,26 @@ public class MainActivity extends AppCompatActivity {
 
         mCountTextView = (TextView) findViewById(R.id.count_textview);
 
-        // 2. 10초 동안 1초에 1씩 카운트 하는 스레드 생성 및 시작
+        // 10초 동안 1초에 1씩 카운트하는 스레드 생성 및 시작
         Thread workerThread = new Thread() {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
                     mCount++;
 
-                    // 3. 실행 코드가 담긴 Runnable 객체를 하나 생성한다.
-                    Runnable callback = new Runnable() {
-                        @Override
-                        public void run() {
-                            // 현재까지 카운트한 수치를 텍스트 뷰에 출력한다.
-                            mCountTextView.setText("Count : " + mCount);
-                        }
-                    };
+                    // 2. 메시지 큐에 담을 메시지를 하나 생성한다.
+                    Message message = Message.obtain(mHandler);
 
-                    // 4. 메시지 큐에 담을 메시지 하나를 생성한다. 생성 시 Runnable 객체를 생성자로 전달한다.
-                    Message message = Message.obtain(mHandler, callback);
+                    // 3. 핸들러의 handleMessage로 전달할 값들을 설정한다.
 
-                    // 5. 핸들러를 통해 메시지를 메시지 큐로 보낸다.
+                    // 무엇을 실행하는 메시지인지 구분하기 위해 구분자 설정
+                    message.what = MESSAGE_DRAW_CONTENT_COUNT;
+                    // 메시지가 실행될 때 참조하는 int형 데이터 설정
+                    message.arg1 = mCount;
+                    // 메시지가 실행될 때 참조하는 Object형 데이터 설정
+                    message.obj = mCountTextView;
+
+                    // 핸들러를 통해 메시지를 메시지 큐로 보낸다.
                     mHandler.sendMessage(message);
 
 
